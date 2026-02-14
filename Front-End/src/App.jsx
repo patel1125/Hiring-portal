53c8241a709d0a55a75e114ed139b97f0f195231
Anupam Katiyar <anupamkatiyar934@gmail.com>
2026-02-14 15:33:54 +0530

    added Forget Password OTP page

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import LoginPage from "./components/LoginPage.jsx";
 
 const App = () => {
@@ -6,6 +7,16 @@ const App = () => {
         <div>
             <LoginPage />
         </div>
+=======
+import Forget_pass from "./Forget_pass.jsx";
+
+const App = () => {
+    return (
+        <>
+        <Forget_pass/>
+        
+        </>
+>>>>>>> 677ec5e (added forget password OTP page)
     );
 };
 
